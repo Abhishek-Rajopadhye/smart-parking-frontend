f82a4d2d475128684d7026036c4d20a8c9fee8dc
Abhishek-Rajopadhye <abhishek.rajopadhye21@gmail.com>
2025-05-08 11:04:27 +0530

    Update Validation.jsx

--- src/pages/Validation.jsx
+++ src/pages/Validation.jsx
@@ -88,7 +88,7 @@ const Validation = () => {
 
 	// Fetch documents on component load
 	useEffect(() => {
-		if (user.email == "abhishek.rajopadhye21@gmail.com" || user.email == "arjunghule6583@gmail.com") {
+		if (user.email == "abhishek.rajopadhye21@gmail.com" || user.email == "arjunghule6583@gmail.com" || user.email == "kalepradeep2001@gmail.com") {
 			fetchDocuments();
 		}
 	}, [user.email]);
