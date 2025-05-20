18f8ea5d7aa5846a4d734176a221663de55f9d36
PradeepKale7 <kalepradeep2001@gmail.com>
2025-05-20 14:32:07 +0530

    docs comment added

--- src/pages/MapSearch.jsx
+++ src/pages/MapSearch.jsx
@@ -7,6 +7,32 @@ import { BACKEND_URL } from "../const";
 import { Box, Button, Drawer, Grid, IconButton, useMediaQuery, useTheme } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
+
+/**
+ *  MapSearch
+ *  This component renders the map view alongside a  sidebar.
+ *  It handles marker fetching, responsive sidebar rendering (drawer for mobile), and integrates
+ *  both MapContainer and MapSidebar components.
+ */
+
+/**
+ * 
+ *
+ * @component
+ * @param {Object} props - Props for MapSearch
+ * @param {Object} props.selectedMarker - Currently selected marker on the map
+ * @param {Function} props.setSelectedMarker - Setter to update selected marker
+ * @param {Object} props.newMarker - Marker from search result
+ * @param {Function} props.setNewMarker - Setter to update new marker
+ * @param {Array} props.markers - All markers fetched from backend
+ * @param {Function} props.setMarkers - Setter to update all markers
+ * @param {Object} props.mapRef - Ref object for Google Map instance
+ * @param {Array} props.filteredMarkers - Markers filtered by sidebar filters
+ * @param {Function} props.setFilters - Setter to update filters from sidebar
+ *
+ * @returns {JSX.Element} The MapSearch component
+ */
+
 const MapSearch = ({
 	selectedMarker,
 	setSelectedMarker,
@@ -22,6 +48,10 @@ const MapSearch = ({
 	const theme = useTheme();
 	const isMobileOrTablet = useMediaQuery(theme.breakpoints.down("md"));
 
+	/**
+	 * Fetch all parking markers from backend on component mount
+	 */
+	
 	useEffect(() => {
 		const fetchMarkers = async () => {
 			try {
@@ -37,6 +67,10 @@ const MapSearch = ({
 
 		fetchMarkers();
 	}, [setMarkers]);
+
+	/**
+	 * Toggle drawer for mobile sidebar
+	 */
 
 	const toggleDrawer = () => {
 		setDrawerOpen(!drawerOpen);
@@ -119,7 +153,7 @@ const MapSearch = ({
 					</Grid>
 				)}
 
-				{/* Map */}
+				{/* Display map section */}
 				<Grid item xs={12} md={9}>
 					<Box
 						sx={{
